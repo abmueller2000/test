d4e24cb1144be7e24351ac6ff7808d449b5adf17
Oliver Ellison, MS SD <89275354+aurelius-in@users.noreply.github.com>
2023-11-05 14:23:33 -0600

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,17 @@ function toggleMenu() {
         content.style.marginLeft = '0';
     }
 }
+
+function loadContent(containerId, contentFile) {
+    fetch(contentFile)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById(containerId).innerHTML = data;
+        })
+        .catch(error => console.error('Error loading content:', error));
+}
+
+// Optional: Initialize with default content
+document.addEventListener('DOMContentLoaded', function() {
+    loadContent('content-container', 'home-content.html');
+});
