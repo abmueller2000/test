014e2bd72618cb895b6b096dbc2ae2ca430daf8b
Oliver Ellison, MS SD <89275354+aurelius-in@users.noreply.github.com>
2023-11-05 13:27:25 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-
+function toggleMenu() {
+    var navigation = document.querySelector('.navigation');
+    navigation.classList.toggle('active');
+    
+    var content = document.querySelector('.main-content');
+    if (navigation.classList.contains('active')) {
+        content.style.marginLeft = '200px';
+    } else {
+        content.style.marginLeft = '0';
+    }
+}
